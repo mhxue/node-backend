f9c1bce3fdd78ad0610d2fab9d72969c60f68fea
daniel <daniel@hotmail.com>
2024-10-28 22:35:17 +0800

    Add Post demo

--- api/version.ts
+++ api/version.ts
@@ -1,14 +1,23 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import { MongoClient } from "mongodb";
+import { MongoClient, MongoServerError } from "mongodb";
 const CONNECTION_STRING = process.env["mogodb_connect_string"]
 
 module.exports = async (req: VercelRequest, res: VercelResponse) => {
-  const client = await MongoClient.connect(CONNECTION_STRING, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const client = await MongoClient.connect(CONNECTION_STRING ?? "");
   const db = await client.db("blog");
-  var result = await db.collection("demo").find().toArray();
-  console.log(result);
-  res.status(200).json(result);
+  if (req.method == 'GET') {
+    var result = await db.collection("demo").find().toArray();
+    res.status(200).json(result);
+  } else if(req.method == 'POST') {
+    try {
+      const newDes = req.body['description']
+      var updateResult = await db.collection("demo").updateOne({ version: "1.0.0" }, { $set: { description: newDes }})
+      res.status(200).json(updateResult);
+    } catch(error) {
+      if (error instanceof MongoServerError) {
+        res.status(405).json('Wrong parameter')
+      }
+      throw error;
+    }
+  }
 };
